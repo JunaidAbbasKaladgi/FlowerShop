b8ea2ab2fbbe7d728a1e000054c643f6f645085b
JunaidAbbas <junaidabbaskaladgi@gmail.com>
2024-11-25 17:46:57 +0530

    Categories section updated

--- Frontend/src/components/Categories.jsx
+++ Frontend/src/components/Categories.jsx
@@ -22,18 +22,18 @@ const Categories = () => {
             <Navbar/>
             <div className='max-w-screen-2xl mt-[12vh] container mx-auto  md:px-20'>
                 <div className=' md:flex'>
-                    <div className='border- gap-x-10 grid grid-cols-2 ml-12 md:gap-[8vh] md:h-[40vh] w-[75%]  md:w-[35%] md:grid-cols-1'>
-                        <div onClick={() => handleCategoryClick('SEASONAL FLOWERS')} className="bg-base-100 md:flex md:w-full md:flex-row md:justify-center w-[15vh] md:items-center md:gap-2 flex flex-col dark:text-black cursor-pointer rounded-xl hover:shadow-x hover:scale-20 duration-500 ">
+                    <div className='border- gap-x-10 grid grid-cols-2 ml-12 md:gap-[8vh]  md:h-[40vh] w-[75%]  md:w-[35%] md:grid-cols-1'>
+                        <div onClick={() => handleCategoryClick('SEASONAL FLOWERS')} className="bg-base-100 md:flex md:w-full relative md:flex-row md:justify-center w-[15vh] md:items-center flex flex-col tex-[#ffffff] cursor-pointer rounded-xl hover:shadow-lg hover:scale-105 duration-500 ">
                             <figure className='md:w-1/2'>
                                 <img className='md:h-[20vh] rounded-xl'
                                     src="https://4.imimg.com/data4/IF/FI/MY-2120688/stock-seasonal-flower-500x500.jpg"
                                     alt="" />
                             </figure>
-                            <div className='md:w-1/2 md:text-left text-center'>
+                            <div className='md:w-1/2 md:text-left  text-center'>
                                 <h2 className=" md:text-[20px] font-normal">SEASONAL FLOWERS</h2>
                             </div>
                         </div>
-                        <div onClick={() => handleCategoryClick('ROSES')} className="w-[15vh] bg-base-100 md:flex md:w-full md:flex-row md:justify-center md:items-center md:gap-2  dark:text-black cursor-pointer rounded-xl hover:shadow-x hover:scale-20 duration-500 ">
+                        <div onClick={() => handleCategoryClick('ROSES')} className="w-[15vh] bg-base-100 md:flex md:w-full md:flex-row md:justify-center md:items-center md:gap-2  dark:text-black cursor-pointer rounded-xl hover:shadow-lg hover:scale-105 duration-500 ">
                             <figure className=' md:w-1/2'>
                                 <img className='md:h-[20vh] rounded-xl'
                                     src="https://floralife.com/wp-content/uploads/2021/12/Roses-DT_2560X1790.png"
@@ -45,7 +45,7 @@ const Categories = () => {
                         </div>
 
 
-                        <div onClick={() => handleCategoryClick('WEDDING FLOWERS')} className="bg-base-100 md:flex md:w-full md:flex-row md:justify-center md:items-center md:gap-2 w-[15vh] dark:text-black cursor-pointer rounded-xl hover:shadow-x hover:scale-20 duration-500 ">
+                        <div onClick={() => handleCategoryClick('WEDDING FLOWERS')} className="bg-base-100 md:flex md:w-full md:flex-row md:justify-center md:items-center md:gap-2 w-[15vh] dark:text-black cursor-pointer rounded-xl hover:shadow-lg hover:scale-105 duration-500">
                             <figure className='md:w-1/2'>
                                 <img className='md:h-[20vh] rounded-xl'
                                     src="https://thumbs.dreamstime.com/b/wedding-flowers-outdoor-25505971.jpg"
@@ -57,7 +57,7 @@ const Categories = () => {
                         </div>
 
 
-                        <div onClick={() => handleCategoryClick('BOUQUETS')} className="bg-base-100 md:flex md:w-full md:flex-row md:justify-center md:items-center md:gap-2 w-[15vh] dark:text-black cursor-pointer rounded-xl hover:shadow-x hover:scale-20 duration-500 ">
+                        <div onClick={() => handleCategoryClick('BOUQUETS')} className="bg-base-100 md:flex md:w-full md:flex-row md:justify-center md:items-center md:gap-2 w-[15vh] dark:text-black cursor-pointer rounded-xl hover:shadow-lg hover:scale-105 duration-500 ">
                             <figure className='md:w-1/2'>
                                 <img className='md:h-[20vh] rounded-xl '
                                     src="https://media.istockphoto.com/id/1159802579/photo/a-lot-of-flower-bouquets-at-the-florist-shop-on-the-table-made-of-hydrangea-roses-peonies.jpg?s=612x612&w=0&k=20&c=bI1YqRYuX_F0E0HSvQM4Zqo2MawIi_-SJ-sWekU681U="
